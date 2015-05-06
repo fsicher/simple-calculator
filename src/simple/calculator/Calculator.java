add0b2da53c66efa2e30ef81f2222f778b127b55
F. Sicher <fsicher@gmail.com>
2015-05-06 19:55:40 +0200

    Methods 'mul' and 'div' added.

--- src/simple/calculator/Calculator.java
+++ src/simple/calculator/Calculator.java
@@ -21,4 +21,26 @@ public class Calculator {
 	public static double sub(double a, double b) {
 		return a-b;
 	}
+	
+	/**
+	 * Calculates the product of a and b.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double mul(double a, double b) {
+		return a*b;
+	}
+	
+	/**
+	 * Calculates the quotient of a and b.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double div(double a, double b) {
+		return a/b;
+	}
+	
+	
 }
