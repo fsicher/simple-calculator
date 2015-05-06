8446cde21712fb6c9c20c3f7ed3e6f9897c8d66c
F. Sicher <fsicher@gmail.com>
2015-05-06 19:52:51 +0200

    Methods 'add' and 'sub' implemented.

---
+++ src/simple/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package simple.calculator;
+
+public class Calculator {
+	
+	/**
+	 * Calculates the sum of a and b.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double add(double a, double b) {
+		return a+b;
+	}
+	
+	/**
+	 * Calculates the difference of a and b.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double sub(double a, double b) {
+		return a-b;
+	}
+}
