662809ebf1dee9a027352e674fc58e880a20ecf7
F. Sicher <fsicher@gmail.com>
2015-05-06 19:58:02 +0200

    Division by zero captured.

--- src/simple/calculator/Calculator.java
+++ src/simple/calculator/Calculator.java
@@ -1,46 +1,55 @@
 package simple.calculator;
 
 public class Calculator {
-	
+
 	/**
 	 * Calculates the sum of a and b.
+	 * 
 	 * @param a
 	 * @param b
 	 * @return
 	 */
 	public static double add(double a, double b) {
-		return a+b;
+		return a + b;
 	}
-	
+
 	/**
 	 * Calculates the difference of a and b.
+	 * 
 	 * @param a
 	 * @param b
 	 * @return
 	 */
 	public static double sub(double a, double b) {
-		return a-b;
+		return a - b;
 	}
-	
+
 	/**
 	 * Calculates the product of a and b.
+	 * 
 	 * @param a
 	 * @param b
 	 * @return
 	 */
 	public static double mul(double a, double b) {
-		return a*b;
+		return a * b;
 	}
-	
+
 	/**
 	 * Calculates the quotient of a and b.
+	 * 
 	 * @param a
 	 * @param b
 	 * @return
 	 */
 	public static double div(double a, double b) {
-		return a/b;
+
+		/* b mustn't be zero */
+		if (b != 0) {
+			return a / b;
+		} else {
+			throw new IllegalArgumentException("divisor mustn't be zero");
+		}
 	}
-	
-	
+
 }
